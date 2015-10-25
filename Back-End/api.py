fcbeabcb68478f3f4e94f15ac9c0ebccd6538628
Akilesh Bapu <akilbapu@sbcglobal.net>
2015-10-24 23:10:30 -0700

    Vantiv API

--- Back-End/api.py
+++ Back-End/api.py
@@ -11,14 +11,13 @@ input_request = {
 "OperatorID":"money2020",
 }
 def transact(cardNumber, purchase, ExpDateString):
-	input_request["AcctNo"] = str(cardNumber)
-	input_request["Purchase"] = str(purchase)
+	input_request["AcctNo"] = cardNumber
+	input_request["Purchase"] = purchase
 	input_request["ExpDate"] = ExpDateString
 	headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Basic MDAzNTAzOTAyOTEzMTA1Onh5eg==',
 	}
-	print(input_request)
-	info = requests.post('https://w1.mercurycert.net/PaymentsAPI/Credit/Sale', headers=headers, params = input_request)
+	info = requests.post('https://w1.mercurycert.net/PaymentsAPI/Credit/Sale', headers=headers, json = input_request)
 	parsed = info.json()
 	print(parsed)
